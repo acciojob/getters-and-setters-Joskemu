16f5fb7255f69e9e0c8763790bc44bedbacecbce
Joskemu <vadhelkemu098@gmail.com>
2025-02-16 11:21:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-//complete this code
-class Person {}
+// Create a base class Person
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-class Student extends Person {}
+    // A method that introduces the person
+    introduce() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
 
-class Teacher extends Person {}
+// Create a Student class that extends Person
+class Student extends Person {
+    constructor(name, age, grade) {
+        super(name, age);  // Call the constructor of Person
+        this.grade = grade;
+    }
+
+    // A method to describe the student's grade
+    study() {
+        console.log(`${this.name} is studying and is in grade ${this.grade}.`);
+    }
+}
+
+// Create a Teacher class that extends Person
+class Teacher extends Person {
+    constructor(name, age, subject) {
+        super(name, age);  // Call the constructor of Person
+        this.subject = subject;
+    }
+
+    // A method to describe the subject the teacher teaches
+    teach() {
+        console.log(`${this.name} is teaching ${this.subject}.`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
